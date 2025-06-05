b716ac18f330b37b8223534de42373e7a725fbe4
Nicolas Pereira <nicolas.pp333@gmail.com>
2025-06-05 17:04:54 -0300

    Refactor escena.h y pared.h

--- headers/escena.h
+++ headers/escena.h
@@ -14,14 +14,24 @@ class Escena_RR
 private:
     Camara_RR camara;
     std::vector<ObjetoPtr> objetos;
-    Color_RR fondo; // Color_RR inicializa en (0, 0, 0) por defecto
+    Color_RR fondo;
 
 public:
     Escena_RR();
-    Color_RR getFondo() { return fondo; }
-    Camara_RR getCamara() { return camara; }
+    Color_RR getFondo();
+    Camara_RR getCamara();
     ObjetoPtr calcularInterseccionMasCercana(Rayo_RR rayo, Vector *punto);
 };
+
+Color_RR Escena_RR::getFondo()
+{
+    return fondo;
+}
+
+Camara_RR Escena_RR::getCamara()
+{
+    return camara;
+}
 
 Escena_RR::Escena_RR()
 {
@@ -36,66 +46,66 @@ Escena_RR::Escena_RR()
 
     ParedPtr paredFondo = std::make_shared<Pared_RR>(
         Vector(10, 0, 0),     // Centro de la pared
-        Vector(-1, 0, 0),     // Normal hacia adelante
+        Vector(-1, 0, 0),     // Normal
         10.0f,                // Ancho de la pared
         10.0f,                // Alto de la pared
-        ColorRGB(255, 0, 0)); // Color rojo
+        ColorRGB(255, 0, 0)); // Color ambiente
     objetos.push_back(paredFondo);
 
     ParedPtr paredIzquierda = std::make_shared<Pared_RR>(
-        Vector(5, 0, -5),     // Centro de la pared
-        Vector(0, 0, 1),      // Normal hacia adelante
-        10.0f,                // Ancho de la pared
-        10.0f,                // Alto de la pared
-        ColorRGB(0, 255, 0)); // Color verde
+        Vector(5, 0, -5),
+        Vector(0, 0, 1),
+        10.0f,
+        10.0f,
+        ColorRGB(0, 255, 0));
     objetos.push_back(paredIzquierda);
 
     ParedPtr paredDerecha = std::make_shared<Pared_RR>(
-        Vector(5, 0, 5),      // Centro de la pared
-        Vector(0, 0, -1),     // Normal hacia adelante
-        10.0f,                // Ancho de la pared
-        10.0f,                // Alto de la pared
-        ColorRGB(0, 0, 255)); // Color azul
+        Vector(5, 0, 5),
+        Vector(0, 0, -1),
+        10.0f,
+        10.0f,
+        ColorRGB(0, 0, 255));
     objetos.push_back(paredDerecha);
 
     ParedPtr paredSuperior = std::make_shared<Pared_RR>(
-        Vector(5, 5, 0),        // Centro de la pared
-        Vector(0, -1, 0),       // Normal hacia adelante
-        10.0f,                  // Ancho de la pared
-        10.0f,                  // Alto de la pared
-        ColorRGB(255, 255, 0)); // Color amarillo
+        Vector(5, 5, 0),
+        Vector(0, -1, 0),
+        10.0f,
+        10.0f,
+        ColorRGB(255, 255, 0));
     objetos.push_back(paredSuperior);
 
     ParedPtr paredInferior = std::make_shared<Pared_RR>(
-        Vector(5, -5, 0),       // Centro de la pared
-        Vector(0, 1, 0),        // Normal hacia adelante
-        10.0f,                  // Ancho de la pared
-        10.0f,                  // Alto de la pared
-        ColorRGB(255, 0, 255)); // Color magenta
+        Vector(5, -5, 0),
+        Vector(0, 1, 0),
+        10.0f,
+        10.0f,
+        ColorRGB(255, 0, 255));
     objetos.push_back(paredInferior);
 
     // TEST Pared frontal, no se debería ver, porque la normal es hacia adentro del cubo
     // ParedPtr paredFrontal = std::make_shared<Pared_RR>(
-    //     Vector(0, 0, 0),        // Centro de la pared
-    //     Vector(1, 0, 0),        // Normal hacia adelante
-    //     10.0f,                  // Ancho de la pared
-    //     10.0f,                  // Alto de la pared
-    //     ColorRGB(0, 255, 255)); // Color cian
+    //     Vector(0, 0, 0),
+    //     Vector(1, 0, 0),
+    //     10.0f,
+    //     10.0f,
+    //     ColorRGB(0, 255, 255));
     // objetos.push_back(paredFrontal);
 
     // TEST Pared paralela a rayo de la cámara no se debería ver, porque la normal es perpendicular al rayo de la cámara
     // ParedPtr paredParalela = std::make_shared<Pared_RR>(
-    //     Vector(5, 0, 0),          // Centro de la pared
-    //     Vector(0, 1, 0),          // Normal hacia adelante
-    //     10.0f,                    // Ancho de la pared
-    //     10.0f,                    // Alto de la pared
-    //     ColorRGB(255, 255, 255)); // Color blanco
+    //     Vector(5, 0, 0),
+    //     Vector(0, 1, 0),
+    //     10.0f,
+    //     10.0f,
+    //     ColorRGB(255, 255, 255));
     // objetos.push_back(paredParalela);
 
     // TEST Pared atrás de la cámara, no se debería ver
     // ParedPtr paredAtras = std::make_shared<Pared_RR>(
     //     Vector(-15, 0, 0),        // Centro de la pared
-    //     Vector(-1, 0, 0),         // Normal hacia adelante
+    //     Vector(-1, 0, 0),         // Normal
     //     10.0f,                    // Ancho de la pared
     //     10.0f,                    // Alto de la pared
     //     ColorRGB(128, 128, 128)); // Color gris
--- headers/pared.h
+++ headers/pared.h
@@ -16,6 +16,7 @@ private:
 public:
     Pared_RR();
     Pared_RR(Vector centro, Vector normal, float ancho, float alto, ColorRGB color);
+    Pared_RR(Vector centro, Vector normal, Vector up, float ancho, float alto, ColorRGB color);
     Vector calcularNormal(Rayo_RR rayo) override;
     bool calcularInterseccion(Rayo_RR rayo, Vector *puntoInterseccion) override;
 };
@@ -44,6 +45,20 @@ Pared_RR::Pared_RR(Vector centro, Vector normal, float ancho, float alto, ColorR
     this->altoVec = ejeY * alto;   // Alto de la pared
 }
 
+Pared_RR::Pared_RR(Vector centro, Vector normal, Vector up, float ancho, float alto, ColorRGB color)
+{
+    this->centro = centro;
+    this->normal = normal;
+    this->colorAmbiente = color;
+
+    // Ejes locales al plano de la pared
+    Vector ejeX = normal.cross(up).normalize(); // Se asume que 'up' es un vector no paralelo a 'normal'
+    Vector ejeY = normal.cross(ejeX).normalize();
+
+    this->anchoVec = ejeX * ancho; // Ancho de la pared
+    this->altoVec = ejeY * alto;   // Alto de la pared
+}
+
 Vector Pared_RR::calcularNormal(Rayo_RR rayo)
 {
     return normal;
@@ -56,24 +71,18 @@ bool Pared_RR::calcularInterseccion(Rayo_RR rayo, Vector *puntoInterseccion)
 
     // Verifica si el rayo es paralelo al plano
     if (glm::abs(denominator) < 1e-6f)
-    {
-        return false; // No hay intersección
-    }
+        return false;
 
     // Verifica si la normal y el rayo apuntan hacia el mismo lado (se ve desde atrás)
     if (denominator > 0)
-    {
-        return false; // El rayo viene desde atrás de la pared
-    }
+        return false;
 
-    // Calcula el parámetro t
+    // Calcula el parámetro t (distancia a la intersección desde el origen del rayo)
     float t = normal.dot(centro - rayo.getOrigen()) / denominator;
 
     // Verifica si la intersección está en la dirección positiva del rayo
-    if (t < 0)
-    {
-        return false; // El plano está detrás del rayo
-    }
+    if (t < 0) // La intersección está detrás del origen del rayo
+        return false;
 
     // Calcula el punto de intersección
     *puntoInterseccion = rayo.getOrigen() + rayo.getDireccion() * t;
